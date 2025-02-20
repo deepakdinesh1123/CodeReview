32c471d547dce3fdafbab9b0a49bb4026c97ada6
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2025-02-20 14:53:33 +0530

    push build

--- action/src/chat.d.ts
+++ action/src/chat.d.ts
@@ -1,7 +1,23 @@
+import { z } from "zod";
+declare const FileReviewResponse: z.ZodObject<{
+    review: z.ZodString;
+    position: z.ZodNumber;
+}, "strip", z.ZodTypeAny, {
+    review: string;
+    position: number;
+}, {
+    review: string;
+    position: number;
+}>;
+type FileReviewType = z.infer<typeof FileReviewResponse>;
 export declare class Chat {
     private openai;
     private isAzure;
-    constructor(apikey: string);
-    private generatePrompt;
-    codeReview: (patch: string) => Promise<string | null>;
+    constructor(apiKey: string);
+    private generateFileReviewUserPrompt;
+    private generatePRSummaryUserPrompt;
+    fileReview(patch: string, filename: string): Promise<FileReviewType | null>;
+    getPRSummary(changedFiles: string): Promise<string>;
+    getCommitReviewsSummary(fileReviews: string): Promise<string>;
 }
+export {};
